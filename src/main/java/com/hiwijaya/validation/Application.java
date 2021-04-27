6e2d5f1d0f725e9a1dd51e59d77d98e812941600
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2021-04-27 17:28:10 +0700

    added spring-boot starter

---
+++ src/main/java/com/hiwijaya/validation/Application.java
@@ -0,0 +1,16 @@
+package com.hiwijaya.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Happy Indra Wijaya
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
